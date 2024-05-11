c1cf052c27402b1167f50637f3f7b1f21341b414
Rocky <993916778@qq.com>
2024-05-12 00:01:20 +0800

    feat:实体查询接口

--- src/main/java/com/hnu/datagraph/controller/GstoreController.java
+++ src/main/java/com/hnu/datagraph/controller/GstoreController.java
@@ -1,15 +1,23 @@
 package com.hnu.datagraph.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hnu.datagraph.common.R;
 import com.hnu.datagraph.entity.GstoreResult;
+import com.hnu.datagraph.entity.Relation;
+import com.hnu.datagraph.entity.vo.Point;
+import com.hnu.datagraph.mapper.RelationMapper;
 import com.hnu.datagraph.service.GstoreService;
-import com.hnu.datagraph.entity.vo.R;
+import com.hnu.datagraph.service.NlpService;
 import io.swagger.annotations.*;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Api(tags = "gstore相关接口")
@@ -20,6 +28,37 @@ public class GstoreController {
 
     @Autowired
     private GstoreService gstoreService;
+
+    @Resource
+    private NlpService nlpService;
+
+    @Resource
+    private RelationMapper relationMapper;
+
+    @GetMapping(value = "/getEntity", produces = "application/json;charset=UTF-8")
+    public R<List<Point>> getEntity(@RequestParam String name) {
+        List<String> structuredQueryList = nlpService.getStructuredQueryList(name);
+        log.info("structuredQueryList:{}", structuredQueryList.toString());
+        List<Point> points = new ArrayList<>();
+        if (structuredQueryList.size() <= 0) {
+            return R.error("抱歉，未查询到相关信息！");
+        } else {
+            String entity = structuredQueryList.get(0);
+            QueryWrapper<Relation> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("SUBJECT", entity);
+            List<Relation> relations = relationMapper.selectList(queryWrapper);
+            if (relations.isEmpty()) {
+                return R.error("抱歉，未查询到相关信息！");
+            }
+
+            points.add(new Point(0, entity, "", -1));
+            for (int i = 1; i <= relations.size(); i++) {
+                Point point = new Point(i, relations.get(i - 1).getObjectItem(), relations.get(i - 1).getRelation(), 0);
+                points.add(point);
+            }
+        }
+        return R.success(points);
+    }
 
     /**
      * sparql查询接口
@@ -52,11 +91,12 @@ public class GstoreController {
             return R.error("数据库连接错误");
         }
         if (res.getStatusCode() == 0) {
-            if (res.getAnsNum() == 0) {
-                R<GstoreResult> r = new R<>(1001, "未查询到结果", res);
-                return r;
-            }
-            return R.<GstoreResult>success("查询成功").data(res);
+//            if (res.getAnsNum() == 0) {
+////                R<GstoreResult> r = new R<>(1001, "未查询到结果", res);
+////                return r;
+//            }
+//            return R.<GstoreResult>success("查询成功").data(res);
+            return R.success(res);
         } else {
             return R.error("查询失败");
         }
